b2ba4d96725bd8e34e5d9ea9b2cd6042565af52f
Imanol <imanolamado@hotmail.com>
2024-11-01 23:01:20 +0100

    commit primerAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AgChartsModule } from 'ag-charts-angular';
+import { AgBarSeriesOptions, AgChartCaptionOptions, AgChartOptions, } from 'ag-charts-community';
+import { ApiService } from './services/api.service';
+
+// Objetos con los datos que queremos recuperar
+//
+// Consulta de visitas por día
+interface consultasDia {
+  fecha : string;
+  consultas : number;
+}
+
+// Consulta de los últimos accesos (usuarios logueados)
+interface ultimosAccesos {  
+  id_usuario : number;
+  fecha: string;
+}
+
+// Consulta número de visitas por pestaña
+interface  visitasPestana {
+  pestana: string;
+  visitas: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, CommonModule, AgChartsModule],
+  template: '<ag-charts [options]="visitasDia"/>'  
 })
-export class AppComponent {
-  title = 'my-app';
+
+
+export class AppComponent implements OnInit {
+  public visitasDia: AgChartOptions;
+
+  // Inyecta el servicio en el constructor
+  constructor(private apiService: ApiService) {
+
+    this.visitasDia = {    
+      title: {
+        text: "Número de visitas por día"
+      } as AgChartCaptionOptions,
+      data: [], // Datos iniciales vacíos
+      series: [
+        {
+          type: "bar",
+          xKey: "fecha",
+          yKey: "pestana"
+        } as AgBarSeriesOptions,
+      ]
+    };
+  }
+
+ 
+  ngOnInit(): void {
+    // llamada a la API
+    this.apiService.getConsultasDias().subscribe(
+      (data: consultasDia[]) => {        
+       
+        this.visitasDia = {
+          ...this.visitasDia, 
+          data: data 
+        };
+      },
+      (error) => {
+        console.error('Error al obtener los datos de la API:', error);
+      }
+    );
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  // NOTA: URL de la api
+  private apiUrl = 'https://localhost:7073';
+
+  constructor(private http: HttpClient) { }
+
+ // Método para obtener datos de /GetConsultasDias
+ getConsultasDias(): Observable<any> {
+  return this.http.get(`${this.apiUrl}/GetConsultasDias`);
+ }
+
+// Método para obtener los datos de /GetPestanaVisitas
+ getPestanaVisitas(): Observable<any> {
+  return this.http.get(`${this.apiUrl}/GetPestanaVisitas`); 
+}
+
+// Método para obtener los datos de /GetPestanaVisitas
+ getUltimosAccesos(): Observable<any> {
+  return this.http.get(`${this.apiUrl}/GetUltimosAccesos`); 
+}
+
+
+}
